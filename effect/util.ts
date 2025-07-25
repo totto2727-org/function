c4cafc1a21462bd93847c8b6c5f8f2bdc27ca642
totto2727 <kaihatu.totto2727@gmail.com>
2025-07-25 20:30:47 +0900

    feat: add Effect.decodeSync helper

--- effect/util.ts
+++ effect/util.ts
@@ -1,7 +1,17 @@
-import { Effect, flow, Function } from "../effect.ts";
+import {
+  Effect,
+  flow,
+  Function,
+  type Schema,
+  type SchemaAST,
+} from "../effect.ts";
 
 export const constVoidEffect = Function.constant(Effect.void);
 
 export const asVoidEffect = <ARGS extends unknown[]>(
   fn: (...args: ARGS) => unknown,
 ) => flow(fn, constVoidEffect);
+
+export type Create<S> = S extends Schema.Schema<infer A, infer I, never>
+  ? (i: I, overrideOptions?: SchemaAST.ParseOptions) => A
+  : never;
