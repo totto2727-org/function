b7e38376c523bcbf93c9688ef8c7e1273990abc8
totto2727 <kaihatu.totto2727@gmail.com>
2025-08-21 16:01:37 +0900

    chore: bump version to 1.5.2

---
+++ effect/cluster.ts
@@ -0,0 +1,12 @@
+/**
+ * Re-exports of `@effect/cluster` modules.
+ *
+ * @example
+ * ```ts
+ * import * as Cluster from "@totto/function/effect/cluster";
+ * ```
+ *
+ * @module
+ */
+
+export * from "npm:@effect/cluster@0.48.2";
---
+++ effect/sql.ts
@@ -0,0 +1,12 @@
+/**
+ * Re-exports of `@effect/sql` modules.
+ *
+ * @example
+ * ```ts
+ * import * as Cluster from "@totto/function/effect/sql";
+ * ```
+ *
+ * @module
+ */
+
+export * from "npm:@effect/sql@0.44.2";
